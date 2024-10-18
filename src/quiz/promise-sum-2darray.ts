10998793fb19703b148f721996c72a0ac4fb6dc1
bzeng68 <zeng.br@northeastern.edu>
2024-10-17 20:58:52 -0400

    animations and async await functionality

--- src/quiz/promise-sum-2darray.ts
+++ src/quiz/promise-sum-2darray.ts
@@ -19,19 +19,32 @@ function rowSum(arr: number[][], rowIndex: number): Promise<number> {
     });
 }
 
-let sumPromises: Promise<number>[] = [];
-for(let x = 0; x < array2D_1.length; x++) {
-    sumPromises.push(rowSum(array2D_1, x));
+async function sumArray(arr: number[][]): Promise<number> {
+    if (arr.length === 0) {
+        throw new Error('no sum for empty array');
+    }
+
+    const sumPromises = arr.map((_, index) => rowSum(arr, index));
+    const rowSums = await Promise.all(sumPromises);
+
+    return rowSums.reduce((acc, curr) => acc + curr);
 }
 
-Promise.all(sumPromises)
-    .then((rowSums) => {
-        let sum = 0;
-        rowSums.forEach(rowSum => {
-            sum += rowSum;
-        });
+async function sumArrayHandler(arr: number[][]): Promise<string> {
+    try {
+        const sum = await sumArray(arr);
         console.log(`Full array sum: ${sum}`);
-    })
-    .catch((error) => {
-        console.log(`Unable to sum 2D array: ${error}`)
-    });
+        return "resolved";
+    } catch (err) {
+        console.log(`Unable to sum 2D array: ${err}`);
+        return "rejected";
+    }
+}
+
+// resolved, 45
+sumArrayHandler(array2D_1)
+    .then(status => console.log(status));
+
+// rejected
+sumArrayHandler([])
+    .then(status => console.log(status));
